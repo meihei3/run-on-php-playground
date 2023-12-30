bd3f515d5c38ff8da99f300db5233dc2f1fe1534
meihei3 <24579635+meihei3@users.noreply.github.com>
2023-12-31 02:48:06 +0900

    add getPhpCodes

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,34 @@
-console.log('ok');
+// PHPコードの正規表現
+const phpCodeRegex: RegExp = /<\?php[\s\S]+?\?>/g;
+
+// 要素からPHPコードを取得する
+function getPhpCodes(element: HTMLElement): string[] {
+    // 要素をクローン
+    const cloneElement: Node = element.cloneNode(true);
+    if (!(cloneElement instanceof HTMLElement)) {
+        return [];
+    }
+
+    // brタグを改行に変換
+    cloneElement.innerHTML = cloneElement.innerHTML.replace(/<br>/g, '\n');
+    // テキストを取得
+    const text = cloneElement.textContent;
+
+    // PHPコードを取得
+    const phpCode = text?.match(phpCodeRegex);
+    if (!phpCode) {
+        return [];
+    }
+
+    // `?>` は削除
+    return phpCode.map((code) => code.replace(/\?>$/, ''));
+}
+
+
+// ページからPHPコードを取得
+const elements: NodeListOf<HTMLElement> = document.querySelectorAll('div.phpcode code');
+elements.forEach((element: HTMLElement) => {
+    getPhpCodes(element).forEach((code) => {
+        console.log(code)
+    });
+});
