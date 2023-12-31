2f036870ab7d759cb385d816c9bf459611f63c78
meihei3 <24579635+meihei3@users.noreply.github.com>
2023-12-31 16:22:26 +0900

    add xo

--- src/components/container.ts
+++ src/components/container.ts
@@ -1,11 +1,11 @@
-import {css} from "@emotion/css";
+import {css} from '@emotion/css';
 
 const styles: string = css`
     position: relative;
 `;
 
 export function createContainer(): HTMLDivElement {
-    const container: HTMLDivElement = document.createElement('div');
-    container.className = styles;
-    return container;
+	const container: HTMLDivElement = document.createElement('div');
+	container.className = styles;
+	return container;
 }
