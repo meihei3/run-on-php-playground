ef12a9b773bdb6b3ebbe765db79fdd462400989b
meihei3 <24579635+meihei3@users.noreply.github.com>
2023-12-31 15:26:08 +0900

    update links that have encoded code

--- src/components/button.ts
+++ src/components/button.ts
@@ -7,7 +7,7 @@ const styles: string = css`
     position: absolute;
     right: 0;
     top: 0;
-    
+
     appearance: none;
     background-color: #2ea44f;
     border: 1px solid rgba(27, 31, 35, .15);
@@ -17,7 +17,7 @@ const styles: string = css`
     color: #fff;
     cursor: pointer;
     display: inline-block;
-    font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
     font-size: 14px;
     font-weight: 400;
     line-height: 20px;
@@ -30,30 +30,36 @@ const styles: string = css`
     vertical-align: middle;
     white-space: nowrap;
 
-    :focus:not(:focus-visible):not(.focus-visible) {
+    :focus:not(:focus-visible) {
         box-shadow: none;
         outline: none;
     }
+
     :hover {
         background-color: #2c974b;
     }
+
     :focus {
         box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
         outline: none;
     }
+
     :disabled {
         background-color: #94d3a2;
         border-color: rgba(27, 31, 35, .1);
         color: rgba(255, 255, 255, .8);
         cursor: default;
     }
+
     :active {
         background-color: #298e46;
         box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
     }
 `;
 
-export function createButton(text: string = defaultText, link: string = defaultLink): HTMLButtonElement {
+export function createButton({text = defaultText, link = defaultLink}: {
+    text?: string, link?: string
+}): HTMLButtonElement {
     const button: HTMLButtonElement = document.createElement('button');
     button.innerText = text;
     button.onclick = () => {
--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,12 @@
 import {createButton} from "./components/button.ts";
 import {createContainer} from "./components/container.ts";
 
+import * as lzstring from 'lz-string';
+
 // PHPコードの正規表現
 const phpCodeRegex: RegExp = /<\?php[\s\S]+?\?>/g;
+// Base URL
+const baseUrl = 'https://php-play.dev/';
 
 // 要素からPHPコードを取得する
 function getPhpCodes(element: HTMLElement): string[] {
@@ -31,16 +35,18 @@ function getPhpCodes(element: HTMLElement): string[] {
 // ページからPHPコードを取得
 const elements: NodeListOf<HTMLElement> = document.querySelectorAll('div.phpcode code');
 elements.forEach((element: HTMLElement) => {
-    // コンテナを作成
-    const container: HTMLDivElement = createContainer();
-    // 元の要素をコンテナに移動
-    element.parentNode?.insertBefore(container, element);
-    container.appendChild(element);
-
-    // ボタンを作成してコンテナに追加
-    element.appendChild(createButton());
-
     getPhpCodes(element).forEach((code) => {
-        console.log(code)
+        // URLを作成
+        const url = new URL(baseUrl);
+        url.searchParams.append('c', lzstring.compressToEncodedURIComponent(code));
+
+        // コンテナを作成
+        const container: HTMLDivElement = createContainer();
+        // 元の要素をコンテナに移動
+        element.parentNode?.insertBefore(container, element);
+        container.appendChild(element);
+
+        // ボタンを作成してコンテナに追加
+        element.appendChild(createButton({link: url.toString()}));
     });
 });
